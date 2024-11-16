076fea5cb6f95cf7f580e9b73374e8bb0254d218
Gipsy Rouaret <gipsy.rouaret@orange.fr>
2024-11-16 10:07:00 +0100

    bouton vers haut et anim btn form

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,70 +1,13 @@
-// const title=document.querySelector("h1");
-// const letters=[...document.querySelectorAll("h1 span")];
+const spanContainers=document.querySelectorAll(".titlecontent div");
 
-// title.addEventListener("mouseenter", handleletters);
-// title.addEventListener("mouseleave", handleletters);
+spanContainers.forEach(item=>{
 
-// let isAnimatingIn=false;
-// let calledOut=false;
-// let animOpened=false;
+      const letters = item.children[0].textContent.split(""); // permet de séparer dans un tableau toutes les lettres du mot
+      item.innerHTML=""; // vide le contenu des élements (div + span) de l'HTML pour créer les futurs span (cf ci-après) 
 
-
-// function handleletters(){
-//       if(animOpened){
-//             animOut();
-//             animOpened=false;
-//             return;
-//       }
-//       if(isAnimatingIn){
-//       calledOut=true;
-//       return;
-//       }
-//       isAnimatingIn=true;
-
-//       const animePromise=new Promise((resolve)=>{
-//             animIn()
-//             setTimeout(()=>{
-//                   resolve()
-//             },750);
-//       });
-//       animePromise.then(()=>{
-//             isAnimatingIn=false;
-//             if(calledOut){
-//             animOut()
-//             calledOut =false;
-//       }else  if(!calledOut){
-//             animOpened=true;
-//       }
-//       })
-// }
-
-// function animIn({
-//       anime({
-//             targets: "h1 span",
-//             translateX: function(){
-//                   return anime.random(-250,250)
-//             };
-//             translateY: function(){
-//                   return anime.random(-250,250)
-//             };
-//             translateZ: function(){
-//                   return anime.random(-2000,750)
-//             };
-//             rotate: function(){
-//                   return anime.random(-250,250)
-//             };
-//             easing:"easeOutCirc",
-//             duration : 750
-//       });
-// });
-// function animOut({
-//       anime({
-//             targets: "h1 span",
-//             translateX: 0,
-//             translateY: 0,
-//             translateZ: 0,
-//             rotate: 0,
-//             easing:"easeInQuad",
-//             duration : 750
-//       });
-// });
+      // ci-dessous : el signifie élement et index : l'index. cette méthode précise que pour chaque lettres un span(entre backtik) est créé dans chaque div 
+      letters.forEach((el,index)=>{
+            item.innerHTML+=`<span style="transition-delay:${0.07*index}s">${el}</span>`
+      })
+      // transition-delay indique la durée à attendre avant de débuter la transition. Ici il attend 0.07s entre chaque lettre. le s signifie que ce style est calculé en secondes.
+})
